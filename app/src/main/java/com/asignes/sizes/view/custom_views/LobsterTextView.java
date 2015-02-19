8549262a4486db17e74de36e62340dd481f1b465
asignes86 <asignes86@gmail.com>
2015-02-19 21:09:38 +0100

    add custom textview

---
+++ app/src/main/java/com/asignes/sizes/view/custom_views/LobsterTextView.java
@@ -0,0 +1,34 @@
+package com.asignes.sizes.view.custom_views;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class LobsterTextView extends TextView {
+  public LobsterTextView(Context context) {
+
+    super(context);
+
+    init(context);
+  }
+
+  public LobsterTextView(Context context, AttributeSet attrs) {
+
+    super(context, attrs);
+
+    init(context);
+  }
+
+  public LobsterTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+
+    super(context, attrs, defStyleAttr);
+
+    init(context);
+  }
+
+  private void init(Context context) {
+    Typeface t = Typeface.createFromAsset(context.getAssets(), "Lobster-Regular.ttf");
+    this.setTypeface(t);
+  }
+}
