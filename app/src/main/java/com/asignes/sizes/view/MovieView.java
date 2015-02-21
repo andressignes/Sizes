39c272da9520f14a5a852e06cf2e23d6754a6ef3
asignes86 <asignes86@gmail.com>
2015-02-21 12:24:33 +0100

    add interface for movieView and item click

---
+++ app/src/main/java/com/asignes/sizes/view/MovieView.java
@@ -0,0 +1,5 @@
+package com.asignes.sizes.view;
+
+public interface MovieView {
+  public android.content.Context getContext();
+}
---
+++ app/src/main/java/com/asignes/sizes/view/OnItemClickListener.java
@@ -0,0 +1,5 @@
+package com.asignes.sizes.view;
+
+public interface OnItemClickListener {
+  public void onClick(View v, int position);
+}
